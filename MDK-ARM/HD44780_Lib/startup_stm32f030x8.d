a8b1a77c6896387bff9341707b7048fa22a340df
mswiecak <M.Swiecak@stud.elka.pw.edu.pl>
2018-12-02 16:38:50 +0100

    initial commit

---
+++ MDK-ARM/HD44780_Lib/startup_stm32f030x8.d
@@ -0,0 +1,1 @@
+hd44780_lib\startup_stm32f030x8.o: startup_stm32f030x8.s
---
+++ MDK-ARM/RTE/_HD44780_Lib/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HD44780_Lib' 
+ * Target:  'HD44780_Lib' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
