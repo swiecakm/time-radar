bf1d8e018e6cd19db0139a2ba121f728058ea563
mswiecak <M.Swiecak@stud.elka.pw.edu.pl>
2019-07-22 13:46:03 +0200

    some comments for AM3202 library

--- Src/AM2302.c
+++ Src/AM2302.c
@@ -13,6 +13,7 @@ void AM2302_SendRequest()
 		AM2302_BitsReceivedLengths[i] = 0;
  	}
 	AM2302_RequestSent = 1;
+	//mimimal star signal length is 1ms
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
 	HAL_Delay(1);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
@@ -22,8 +23,14 @@ void AM2302_SendRequest()
 void AM2302_TranslateSignalLengths()
 {
 	uint64_t bitValue;
+	//first two falling edges in signal are:
+	//1 - edge from host pulldown to request data from sensor
+	//2 - edge from sensor pulldown to signalize transmission start
 	for(int i=2; i<42; i++)
 	{
+		//for bit 0: 50us low + 26us high = 76us
+		//for bit 1: 50us low + 70us high = 120us
+		//AM2302_BitsReceivedLengths is in us
 		if(AM2302_BitsReceivedLengths[i] < 100)
 		{
 			bitValue = 0;
@@ -35,6 +42,7 @@ void AM2302_TranslateSignalLengths()
 		
 		uint64_t bitPosition = 41 - i;
 		uint64_t mask = ((uint64_t)1) << bitPosition; 
+		//i do not check checksum but there is function available for this
 		AM2302_ReceivedData = (AM2302_ReceivedData & ~mask) | ((bitValue << bitPosition) & mask);
 	}
 }
@@ -57,6 +65,7 @@ void HAL_TIM_IC_CaptureCallback (TIM_HandleTypeDef *htim)
 		
 		if(AM2302_BitsReceivedCount == 42)
 		{
+			//i do this at last bit received to keep this callback light during transmission
 			AM2302_TranslateSignalLengths();
 		}
 		__HAL_TIM_SET_COUNTER(&htim3, 0);
@@ -66,12 +75,15 @@ void HAL_TIM_IC_CaptureCallback (TIM_HandleTypeDef *htim)
 uint16_t AM2302_GetHumidity()
 {
 	uint64_t mask = 0xFFFF;
+	//to get humidity divide this value by 10
 	return (mask & (AM2302_ReceivedData >> 24));
 }
 
 uint16_t AM2302_GetTemperature()
 {
 	uint64_t mask = 0xFFFF;
+	//the sign is the highest bit
+	//to get temperatrue divide this value by 10
 	return (mask & (AM2302_ReceivedData >> 8));
 }
 
